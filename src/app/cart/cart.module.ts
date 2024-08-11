7118bcb11d8faa683163b6b90bea7ff7fb0b5236
dinh2350 <hao9x0159@gmail.com>
2024-08-11 12:07:13 +0700

    updated routes

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart.component';
-
-
+import { RouterModule } from '@angular/router';
+import { cartRoutes } from './cart.routes';
 
 @NgModule({
-  declarations: [
-    CartComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CartComponent],
+  imports: [CommonModule, RouterModule.forChild(cartRoutes)],
 })
-export class CartModule { }
+export class CartModule {}
