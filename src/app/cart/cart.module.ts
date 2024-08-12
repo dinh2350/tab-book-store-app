c1687381b4b905dcadcce8dfe7bb2fe7c9bf0bd9
dinh2350 <hao9x0159@gmail.com>
2024-08-12 16:44:22 +0700

    updated UI for book and cart

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CartComponent } from './cart.component';
 import { RouterModule } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+
+import { CartComponent } from './cart.component';
 import { cartRoutes } from './cart.routes';
+import { ShareModule } from 'src/share/share.module';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 @NgModule({
-  declarations: [CartComponent],
-  imports: [CommonModule, RouterModule.forChild(cartRoutes)],
+  declarations: [CartComponent, CartItemComponent],
+  imports: [
+    CommonModule,
+    ShareModule,
+    RouterModule.forChild(cartRoutes),
+    MatCheckboxModule,
+    MatCardModule,
+  ],
 })
 export class CartModule {}
