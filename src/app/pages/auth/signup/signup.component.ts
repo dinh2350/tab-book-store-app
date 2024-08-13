c813ab0181d15733526e0050baa2cb2dd0e0ee9f
Hao Nguyen <hao.nguyenphong@nashtechglobal.com>
2024-08-13 10:19:10 +0700

    updated ui for auth

--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-signup',
+//   templateUrl: './signup.component.html',
+//   styleUrls: ['./signup.component.scss']
+// })
+// export class SignupComponent {
+
+// }
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signUpForm!: FormGroup;
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      const { email, password } = this.signUpForm.value;
+      console.log('Sign Up:', { email, password });
+      // Add your sign-up logic here
+    } else {
+      this.errorMessage = 'Please fill out the form correctly.';
+    }
+  }
 }
